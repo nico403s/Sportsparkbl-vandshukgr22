401e1adc72b62cbc18fd4b7920f08a4dc06a96a4
MadsKarstoft <mads394@gmail.com>
2019-05-20 14:18:28 +0200

    Laver en select menu til antal dage og antal personer med html og javascript
    Har også prøvet at lave autofill-funktionalitet, men jeg tror ikke det fungerer. Ved i hvert fald ikke hvad der "trigger" auto-fill i en browser. Gør alle input til "required".

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+for(let i=1; i<=99; i++){
+  let select = document.getElementById("select_antal_personer_page2");
+  let option = document.createElement('option');
+  select.options.add(option);
+  option.text = i;
+  option.value = i;
+}
+
+for(let i=1; i<=7; i++){
+  let select = document.getElementById("select_antal_dage");
+  let option = document.createElement('option');
+  select.options.add(option);
+  option.text = i;
+  option.value = i;
+}
