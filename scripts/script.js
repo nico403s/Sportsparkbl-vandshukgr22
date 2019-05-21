e599991fa32d12a8288876f1f0e30a614598b618
MadsKarstoft <mads394@gmail.com>
2019-05-21 09:50:48 +0200

    Sætter CSS sammen, ændrer på skriftstørrelse og ændrer på størrelse af bookingtilbudcontainerne på page2

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,11 @@ for(let i=1; i<=7; i++){
   option.text = i;
   option.value = i;
 }
+
+for(let i=1; i<=99; i++){
+  let select = document.getElementById("select_antal_personer_page3");
+  let option = document.createElement('option');
+  select.options.add(option);
+  option.text = i;
+  option.value = i;
+}
