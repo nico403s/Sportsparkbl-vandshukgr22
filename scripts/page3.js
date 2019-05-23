3ae1e639404c04d6fb5a1a040589046d11b4a45d
MadsKarstoft <mads394@gmail.com>
2019-05-23 13:02:41 +0200

    Samler Javascript i en JS-fil
    Laver if-statements, eftersom koden ikke fungerer helt rigtigt på page3, uden de if-statements jeg har tilføjet. Der er sikkert en bedre metode uden if-statements, men det var den eneste løsning jeg kunne finde på, der fungerede.

--- scripts/page3.js
+++ scripts/page3.js
@@ -1,7 +1,1 @@
-for(let i=1; i<=99; i++){
-  let select = document.getElementById("select_antal_personer_page3");
-  let option = document.createElement('option');
-  select.options.add(option);
-  option.text = i;
-  option.value = i;
-}
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,32 @@
+let page2Body = document.getElementById('page2_body')
+if (page2Body) {
 for(let i=1; i<=99; i++){
   let select = document.getElementById('select_antal_personer_page2');
   let option = document.createElement('option');
   select.options.add(option);
   option.text = i;
   option.value = i;
-}
+}}
 
+if (page2Body) {
 for(let i=1; i<=7; i++){
   let select = document.getElementById('select_antal_dage');
   let option = document.createElement('option');
   select.options.add(option);
   option.text = i;
   option.value = i;
-}
+}}
+
+let page3Body = document.getElementById('page3_body')
+if (page3Body) {
+for(let i=1; i<=99; i++){
+  let select = document.getElementById("select_antal_personer_page3");
+  let option = document.createElement('option');
+  select.options.add(option);
+  option.text = i;
+  option.value = i;
+}}
+
 
 /* Popupgalleri. Lader det være en kommentar, så det stadig kan bruges, hvis vi vil bruge det senere. Men ligenu er det ikke færdigt og derfor skal det ikke være synligt på siden ligenu.
 function badelandFunction() {
